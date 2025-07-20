b35fea1644c8aa5c600f6e04403741cc1d5a9c80
wenbin <1607764432@qq.com>
2025-07-21 01:52:49 +0800

    init

---
+++ src/main/java/edu/whpu/controller/HttpClientController.java
@@ -0,0 +1,42 @@
+package edu.whpu.controller;
+
+import com.alipay.sofa.tracer.plugins.httpclient.SofaTracerHttpClientBuilder;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class HttpClientController {
+    @GetMapping("/demo")
+    public String demo() throws IOException {
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
+        connManager.setDefaultMaxPerRoute(6);
+        connManager.setMaxTotal(20);
+        connManager.closeIdleConnections(120, TimeUnit.SECONDS);
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        // SOFATracer
+        SofaTracerHttpClientBuilder.clientBuilder(httpClientBuilder);
+        CloseableHttpClient httpClient = httpClientBuilder.setConnectionManager(connManager).disableAutomaticRetries()
+                .build();
+        // 调用9090端口的服务
+        CloseableHttpResponse response1 = httpClient.execute(new HttpGet("http://localhost:9090/demo"));
+        // 调用9091端口的服务
+        CloseableHttpResponse response2 = httpClient.execute(new HttpGet("http://localhost:9091/demo"));
+        new Thread(() -> {
+            try {
+                httpClient.execute(new HttpGet("http://localhost:9091/demo"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        return EntityUtils.toString(response1.getEntity());
+    }
+}
